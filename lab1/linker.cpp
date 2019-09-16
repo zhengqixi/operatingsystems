c953b360027eec1e536f675b3700914888d481fc
zxi8 <zxi8@bloomberg.net>
2019-09-16 18:25:39 -0400

    Basic case works

--- lab1/linker.cpp
+++ lab1/linker.cpp
@@ -1,9 +1,12 @@
 #include "linker.h"
 #include "SymbolTable.h"
 #include "parser.h"
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <utility>
+#include <vector>
 namespace NYU {
 namespace OperatingSystems {
     Linker::Linker(std::string fileName, std::ostream& output)
@@ -32,14 +35,14 @@ namespace OperatingSystems {
             int useListSize = std::stoi(parseIgnoreEmpty(parse));
             for (int i = 0; i != useListSize; ++i) {
                 // Just parse it for now
-                parse.parseToken();
+                parseIgnoreEmpty(parse);
             }
             int instructionSize = std::stoi(parseIgnoreEmpty(parse));
-            absoluteAddress += instructionSize;
             for (int i = 0; i != instructionSize; ++i) {
                 // Just parse it for now
                 getSymbol(parse);
             }
+            absoluteAddress += instructionSize;
         }
         return table;
     }
@@ -66,8 +69,54 @@ namespace OperatingSystems {
     {
         Parser parse(d_fileName);
         d_output << symtable;
+        d_output << "\nMemory Map\n";
+        int absoluteAddress = 0;
+        int currentAddress = 0;
+        int moduleNumber = 1;
         while (parse.continueParsing()) {
-            parse.parseToken();
+            int defineSize = std::stoi(parseIgnoreEmpty(parse));
+            for (int i = 0; i != defineSize; ++i) {
+                getSymbol(parse);
+            }
+            std::vector<std::string> useList;
+            int useListSize = std::stoi(parseIgnoreEmpty(parse));
+            for (int i = 0; i != useListSize; ++i) {
+                auto useSymbol = parseIgnoreEmpty(parse);
+                useList.push_back(useSymbol);
+            }
+            int instructionSize = std::stoi(parseIgnoreEmpty(parse));
+            for (int i = 0; i != instructionSize; ++i) {
+                auto instruction = getSymbol(parse);
+                int opcode = instruction.second / 1000;
+                int operand = instruction.second % 1000;
+                char addressMode = instruction.first.at(0);
+                switch (addressMode) {
+                case 'R':
+                    operand += absoluteAddress;
+                    break;
+                case 'E': {
+                    auto useSymbol = useList[operand];
+                    operand = symtable.getSymbol(useSymbol);
+                } break;
+                case 'I':
+                    // Do nothing
+                    break;
+                case 'A':
+                    // Do nothing
+                    break;
+                }
+                std::stringstream addressFormatter;
+                addressFormatter << std::setw(4) << std::setfill('0');
+                addressFormatter << currentAddress;
+                std::string addressString;
+                addressFormatter >> addressString;
+                d_output << addressString << ": ";
+                int newInstructionCode = opcode * 1000 + operand;
+                d_output << newInstructionCode << '\n';
+                ++currentAddress;
+            }
+            absoluteAddress += instructionSize;
+            ++moduleNumber;
         }
     }
 }
